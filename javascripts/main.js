24ba4bd576add0826e529359365df35a06b77efd
jmarcusgill <jmarcusgill@gmail.com>
2017-05-09 20:49:53 -0500

    switch views

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,13 +1,29 @@
 var app = angular.module("TodoApp", []);
 
-  app.controller("NavCtrl", ($scope) => { //iify that controls navbar in html
-  //$scope connects to dom
+app.controller("NavCtrl", ($scope) => { //iify that controls navbar in html
+//$scope connects to dom
 
-    $scope.cat = "Meow";
-    $scope.navItems = [{name: "Logout"}, {name: "All Items"}, {name: "New Item"}]; //must be same key
-  });
+  $scope.cat = "Meow";
+  $scope.navItems = [{name: "Logout"}, {name: "All Items"}, {name: "New Item"}]; //must be same key
+});
 
-  app.controller("ItemCtrl", ($scope) => {
-    $scope.dog = "Woof!";
-  });
+app.controller("ItemCtrl", ($scope) => {
+  $scope.dog = "Woof!";
+
+  $scope.showListView = true; //this is what is read on page load
+
+  $scope.newItem = () => {
+    $scope.showListView = false;
+    console.log("new item");
+  };
+
+  $scope.allItems = () => {
+    $scope.showListView = true;
+    console.log("all items");
+  };
+
+
+});
+
+
 
