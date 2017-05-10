433be2a89e353384ffb306a6e7dc93ba891780eb
jmarcusgill <jmarcusgill@gmail.com>
2017-05-09 19:48:38 -0500

    added buttons dynamically with angular

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,13 @@
-console.log("totally working");
+var app = angular.module("TodoApp", []);
+
+  app.controller("NavCtrl", ($scope) => { //iify that controls navbar in html
+  //$scope connects to dom
+
+    $scope.cat = "Meow";
+    $scope.navItems = [{name: "Logout"}, {name: "All Items"}, {name: "New Item"}]; //must be same key
+  });
+
+  app.controller("ItemCtrl", ($scope) => {
+    $scope.dog = "Woof!";
+  });
+
